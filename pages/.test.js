4b635fdff442c2ab18f4b3255aed4c1b4a45e3ba
Mrmagic12 <157940462+Mrmagic12@users.noreply.github.com>
2024-05-04 09:44:31 +0000

    style: Remove unnecessary Jest configuration

--- pages/.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import IndexPage from "./index";
-
-test("renders the index page", () => {
-  render(<IndexPage />);
-  const titleElement = screen.getByText(/welcome to my app/i);
-  expect(titleElement).toBeInTheDocument();
-});
